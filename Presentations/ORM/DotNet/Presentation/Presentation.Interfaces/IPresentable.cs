0af690686a18fcaf5bfacc944028149d3bb20669
Volodymyr Novak <Volodymyr Novak>
2017-05-12 19:58:57 +0300

    Added: ORM Presentation EF Database First

---
+++ Presentations/ORM/DotNet/Presentation/Presentation.Interfaces/IPresentable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Interfaces
+{
+    public interface IPresentable
+    {
+        void Add10000Entities();
+        void Delete1000Entities();
+        void Update1000Entities();
+        void Where();
+    }
+}
