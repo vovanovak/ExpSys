51c33b84fd6d8561e3575b02df79d9b0baf20f96
Dima Lytvyniuk <Dima Lytvyniuk>
2017-05-22 22:21:36 +0300

    Fix examples for hibernate

---
+++ Presentations/ORM/Java/TestHibernateProject/src/main/java/TestHibernate/ExpertEntity.java
@@ -0,0 +1,55 @@
+package TestHibernate;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+
+@Entity
+@Table(name = "Expert", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "ID"),
+        @UniqueConstraint(columnNames = "EMAIL") })
+public class ExpertEntity implements Serializable {
+    private static final long serialVersionUID = -1798070786993154676L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID", unique = true, nullable = false)
+    private Integer expertId;
+
+    @Column(name = "EMAIL", unique = true, nullable = false, length = 45)
+    private String email;
+
+    @Column(name = "NAME", unique = false, nullable = false, length = 45)
+    private String name;
+
+    public Integer getExpertId() {
+        return expertId;
+    }
+
+    public void setExpertId(Integer expertId) {
+        this.expertId = expertId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- Presentations/ORM/Java/TestHibernateProject/src/main/java/TestHibernate/Test.java
+++ Presentations/ORM/Java/TestHibernateProject/src/main/java/TestHibernate/Test.java
@@ -8,7 +8,7 @@ public class Test {
     public static void main(String[] args) {
         Session session = HibernateUtil.getSessionFactory().openSession();
         session.beginTransaction();
-
+        /*
         //Add new Employee object
         EmployeeEntity emp = new EmployeeEntity();
         emp.setEmail("petia@gmail.com");
@@ -17,8 +17,29 @@ public class Test {
 
         session.save(emp);
 
+
+        ExpertEntity expEnt = new ExpertEntity();
+        expEnt.setEmail("michael@gmail.com");
+        expEnt.setName("Michael_228");
+        session.save(expEnt);
         session.getTransaction().commit();
-        List<EmployeeEntity> list = session.createCriteria(EmployeeEntity.class).list();
+
+
+
+        OpinionEntity opinionEntity = new OpinionEntity();
+        opinionEntity.setDiscussionId(1);
+        opinionEntity.setExpertId(3);
+        opinionEntity.setOpinionText("It's better than Prometheus");
+        session.save(opinionEntity);
+        session.getTransaction().commit();
+*/
+
+        DiscussionEntity discussionEntity = new DiscussionEntity();
+        discussionEntity.setName("About new Alien");
+        session.save(discussionEntity);
+        session.getTransaction().commit();
+
+        //List<EmployeeEntity> list = session.createCriteria(EmployeeEntity.class).list();
         HibernateUtil.shutdown();
     }
 }
